19c77e62b279fa79a1f179904d167f977b4a62b2
Alexandr Lisseyenko <krisstalexx@gmail.com>
2026-06-27 21:21:36 +0200

    Add initial implementation of 'hi-this-is-brain' program with memory address and value demonstration

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::string  str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "The memory address of str: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of str: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	return 0;
+}
